6ddd4e914bdee75ac3b914c39d53ca0db5534666
Michael West <michaelpwest@users.noreply.github.com>
2019-07-01 21:38:44 -0400

    - Added basic UI structure. - Added stopwatch view. - Added stopwatch and music player components. - Updated Tizen app config so screen will stay on.

---
+++ src/views/Stopwatch.vue
@@ -0,0 +1,17 @@
+<template>
+	<section>
+		<Stopwatch />
+		<MusicPlayer />
+	</section>
+</template>
+
+<script>
+import Stopwatch from "@/components/Stopwatch.vue";
+import MusicPlayer from "@/components/MusicPlayer.vue";
+export default {
+	components: {
+		Stopwatch,
+		MusicPlayer,
+	},
+};
+</script>
